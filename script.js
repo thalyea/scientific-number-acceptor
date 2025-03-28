ea179540696161daa42328bfaed86bb33d2f24c3
thalyea <thalia.dumingan2006@gmail.com>
2025-03-28 09:17:22 +0530

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ function validateNumber() {
         message.style.color = "red";
     }
 }
+function showAbout() {
+    document.getElementById("aboutModal").style.display = "block";
+}
+
+function closeAbout() {
+    document.getElementById("aboutModal").style.display = "none";
+}
